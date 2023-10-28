3bf6f75dfc8572a7b735d52c3afc07850f01664b
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-28 04:44:15 -0700

    task wip

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,9 @@
 index.py File that defines a view function
 on route  "/status"
 """
-from flask import jsonify
+from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 # create a route /status on the object app_views
 # that returns a JSON: "status": "OK"
 
@@ -12,6 +13,6 @@ from api.v1.views import app_views
 @app_views.route('/status', strict_slashes=False)
 def get_app_status():
     """ return status ok in json
-    """
+    for task 3"""
 
     return (jsonify({"status": "OK"}))
